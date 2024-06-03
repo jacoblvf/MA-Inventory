4334f9197ee2b3bfdd63752e5dc50004a25785e9
jacoblvf <163808026+jacoblvf@users.noreply.github.com>
2024-06-03 15:42:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,29 +8,32 @@ df1 = requests.get('https://api.orcascan.com/sheets/f5xG1-gqdcueAPfe?datetimefor
 df2 = requests.get('https://api.orcascan.com/sheets/rt7SbnAGBhSmb7EU?datetimeformat=DD/MM/YYYY HH:mm:ss&timezone=+00:00:').content
 df3 = pd.read_csv(io.StringIO(df1.decode('utf-8')))
 df4 = pd.read_csv(io.StringIO(df2.decode('utf-8')))
-df5 = df3.sort_values(by='Barcode', ascending=False)
-df6 = df4.sort_values(by='Barcode', ascending=False)
-df7 = pd.concat([df3,df4["Scan_out"]], axis=1)
-if df7.index.name is None:
-    df7 = df7[df7.index.notnull()]
-df7["scan_qty"] = df7["Scan_in"] - df7["Scan_out"]
-df7["indiv_qty"] = df7["scan_qty"]*df7["Multiplier"]
-df8 = df7.groupby(["Name"])[["Bulk_or_Indiv", "indiv_qty"]].agg(bulkindiv = ("Bulk_or_Indiv", lambda x:"Indiv"), qty = ("indiv_qty", "sum"))
+df5_2 = df3.merge(df4, on=['Name', 'Bulk_or_Indiv'], suffixes=[None, '_copy'])
+df3 = df3.sort_values(by='Name', ascending=False)
+df4 = df4.sort_values(by='Name', ascending=False)
+df5 = pd.concat([df3,df4["Scan_out"]], axis=1)
+df5=df5_2
+df5 = df5[df5['Barcode'] != "0"]
+if df5.index.name is None:
+    df5 = df5[df5.index.notnull()]
+df5["scan_qty"] = df5["Scan_in"] - df5["Scan_out"]
+df5["indiv_qty"] = df5["scan_qty"]*df5["Multiplier"]
+df6 = df5.groupby(["Name"])[["Bulk_or_Indiv", "indiv_qty"]].agg(bulkindiv = ("Bulk_or_Indiv", lambda x:"Indiv"), qty = ("indiv_qty", "sum"))
 
-df9 = df8.rename(columns={'bulkindiv': 'Status', 'qty': 'Product Quantity'})
-
-st.dataframe(df9, width=1000, height=600)
+df7 = df6.rename(columns={'bulkindiv': 'Status', 'qty': 'Product Quantity'})
+df7['Product Quantity'] = df7['Product Quantity'].astype("int64")
+st.dataframe(df7, width=1000, height=600)
 
 refresh_button = st.button("Refresh")
 if refresh_button:
     st.experimental_rerun()
     
 @st.cache_data
-def convert_df(df9):
+def convert_df(df7):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df9.to_csv().encode('utf-8')
+    return df7.to_csv().encode('utf-8')
 
-csv = convert_df(df9=df9)
+csv = convert_df(df7=df7)
 
 st.download_button(
     label="Download data as CSV",
