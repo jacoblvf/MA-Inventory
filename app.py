22e169aa5c5b1c81568f6c9a383f8724fe5d43cd
jacoblvf <163808026+jacoblvf@users.noreply.github.com>
2024-06-03 16:54:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,17 @@ df1 = requests.get('https://api.orcascan.com/sheets/f5xG1-gqdcueAPfe?datetimefor
 df2 = requests.get('https://api.orcascan.com/sheets/rt7SbnAGBhSmb7EU?datetimeformat=DD/MM/YYYY HH:mm:ss&timezone=+00:00:').content
 df3 = pd.read_csv(io.StringIO(df1.decode('utf-8')))
 df4 = pd.read_csv(io.StringIO(df2.decode('utf-8')))
+df5_2 = df3.merge(df4, on=['Name', 'Bulk_or_Indiv'], suffixes=[None, '_copy'])
 df3 = df3.sort_values(by='Name', ascending=False)
 df4 = df4.sort_values(by='Name', ascending=False)
 df5 = pd.concat([df3,df4["Scan_out"]], axis=1)
+df5=df5_2
 df5 = df5[df5['Barcode'] != "0"]
 if df5.index.name is None:
     df5 = df5[df5.index.notnull()]
 df5["scan_qty"] = df5["Scan_in"] - df5["Scan_out"]
 df5["indiv_qty"] = df5["scan_qty"]*df5["Multiplier"]
-df6 = df5.groupby(["Name"])[["Bulk_or_Indiv", "indiv_qty"]].agg(bulkindiv = ("Bulk_or_Indiv", lambda x:"Indiv"), qty = ("indiv_qty", "sum"))
+df6 = df5.groupby(["Name", "Bulk_or_Indiv"])[["Bulk_or_Indiv", "indiv_qty"]].agg(bulkindiv = ("Bulk_or_Indiv", lambda x:"Indiv"), qty = ("indiv_qty", "sum"))
 
 df7 = df6.rename(columns={'bulkindiv': 'Status', 'qty': 'Product Quantity'})
 df7['Product Quantity'] = df7['Product Quantity'].astype("int64")
